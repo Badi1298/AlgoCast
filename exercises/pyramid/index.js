051cda1a4625aecbbaf65fae2e945bdbdec2fcc0
Badi1298 <serbandavid83@yahoo.com>
2024-07-16 21:43:40 +0300

    042

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,87 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// 1.
+// function pyramid(n) {
+// 	for (let row = 1; row <= n; row++) {
+// 		let pyramid = '';
+
+// 		for (let col = 1; col <= n; col++) {
+// 			if (col <= row) {
+// 				pyramid += '#';
+// 				continue;
+// 			}
+
+// 			pyramid += ' ';
+// 		}
+
+// 		const reversedPyramid = pyramid.split('').reverse().join('');
+// 		pyramid = reversedPyramid.slice(0, reversedPyramid.length - 1) + pyramid;
+
+// 		console.log(pyramid);
+// 	}
+// }
+
+// 2.
+// function pyramid(n, row = 0, myPyramid = '') {
+// 	if (row === n) return;
+
+// 	if (myPyramid.length === n) {
+// 		const reversedPyramid = myPyramid.split('').reverse().join('');
+// 		myPyramid = reversedPyramid.slice(0, reversedPyramid.length - 1) + myPyramid;
+
+// 		console.log(myPyramid);
+// 		return pyramid(n, row + 1);
+// 	}
+
+// 	if (myPyramid.length <= row) {
+// 		myPyramid += '#';
+// 	} else {
+// 		myPyramid += ' ';
+// 	}
+
+// 	pyramid(n, row, myPyramid);
+// }
+
+// 3.
+// function pyramid(n) {
+// 	const midPoint = Math.floor((n * 2 - 1) / 2);
+
+// 	for (let row = 0; row < n; row++) {
+// 		let pyramid = '';
+
+// 		for (let col = 0; col < n * 2 - 1; col++) {
+// 			if (midPoint - row <= col && midPoint + row >= col) {
+// 				pyramid += '#';
+// 			} else {
+// 				pyramid += ' ';
+// 			}
+// 		}
+
+// 		console.log(pyramid);
+// 	}
+// }
+
+// 4.
+function pyramid(n, row = 0, level = '') {
+	if (row === n) return;
+
+	if (level.length === n * 2 - 1) {
+		console.log(level);
+		return pyramid(n, row + 1);
+	}
+
+	const midPoint = Math.floor((n * 2 - 1) / 2);
+
+	let add;
+
+	if (midPoint - row <= level.length && midPoint + row >= level.length) {
+		add = '#';
+	} else {
+		add = ' ';
+	}
+
+	pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,35 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// 1.
+// function steps(n) {
+// 	for (let i = 1; i <= n; i++) {
+// 		let stair = '';
+
+// 		for (j = 1; j <= n; j++) {
+// 			if (j <= i) {
+// 				stair += '#';
+// 				continue;
+// 			}
+
+// 			stair += ' ';
+// 		}
+
+// 		console.log(stair);
+// 	}
+// }
+
+// 2.
+function steps(n, row = 0, stair = '') {
+	if (row === n) return;
+
+	if (stair.length === n) {
+		console.log(stair);
+		return steps(n, row + 1);
+	}
+
+	const add = stair.length <= row ? '#' : ' ';
+	steps(n, row, stair + add);
+}
 
 module.exports = steps;
